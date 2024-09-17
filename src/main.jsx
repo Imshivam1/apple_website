f012fefcd060a8db9bd7ed67e813eba92d1c7a4b
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-09-17 15:26:01 +0530

    updated sentry and animations

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,29 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://739374f6650f60217572b4b712e722c2@o4507967552225280.ingest.us.sentry.io/4507967555502080",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect : React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
