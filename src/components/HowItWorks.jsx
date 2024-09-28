3d2aa8a46c7025026e276255b2e01e315bf869b4
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-09-28 08:24:08 +0530

    added feature

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { chipImg } from '../utils'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
 
 const HowItWorks = () => {
+    useGSAP(() => {
+        gsap.from('#chip', {
+          scrollTrigger: {
+            trigger: '#chip',
+            start: '20% bottom'
+          },
+          opacity: 0,
+          scale: 2,
+          duration: 2,
+          ease: 'power2.inOut'
+        })
+    }, []);
   return (
-    <div>HowItWorks</div>
+    <section className='common-padding '>
+        <div className='screen-max-width'>
+            <div id= 'chip' className='flex-center w-full my-20'>
+                <img src={chipImg} alt='chip' width={180} height={180}/>
+            </div>
+        </div>
+    </section>
   )
 }
 
